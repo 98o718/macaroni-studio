5cbef55355658eb2a92fb661650b71db71b8c0ec
Max Kalyakin <98o718@MacBook-Pro-Max.local>
2019-03-30 19:30:20 +0300

    Adding title

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 
 import './Layout.scss';
 
 const Layout = () => {
   return (
     <main className="layout">
+      <Head>
+        <title>macaroni.studio</title>
+      </Head>
       <img
         className="layout__logo"
         src="/static/images/logo.png"
