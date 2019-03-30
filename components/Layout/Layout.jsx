d5dd8d5e93eed685e4f18252e236d43031d33a2e
Max Kalyakin <98o718@MacBook-Pro-Max.local>
2019-03-30 15:50:28 +0300

    Init commit

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Layout.scss';
+
+const Layout = () => {
+  return (
+    <main className="layout">
+      <img
+        className="layout__logo"
+        src="/static/images/logo.png"
+        alt="Macaroni Studio логотип"
+      />
+    </main>
+  );
+};
+
+export default Layout;
